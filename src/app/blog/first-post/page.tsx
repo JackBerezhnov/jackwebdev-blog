ffab751eef932720eba9ae65cd6bea730542ae41
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-03-25 21:14:59 -0700

    Create a first post

---
+++ src/app/blog/first-post/page.tsx
@@ -0,0 +1,11 @@
+export default function FirstPost() {
+    return (
+        <header className="flex justify-center">
+        <h1>First Post</h1>
+        <div className="description">
+            <p>This is my first blog post.</p>
+            <p>Here you will find articles about programming and technology.</p>
+        </div>
+        </header>
+    );
+}
