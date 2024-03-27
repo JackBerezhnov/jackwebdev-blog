d6d4e2c30a06f0aeba5db90ab06e2b01b6e91d1a
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-03-26 21:59:39 -0700

    Add some text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <header className="flex justify-center">
-      <h1>Blog</h1>
+      <h1>Blog Today</h1>
       <div className="description">
         <p>Hi, Welcome to my Blog!</p>
         <p>Here you will find articles about programming and technology.</p>
