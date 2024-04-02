4d13619bda1d596bf3c50344d27b5e587666d726
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-01 22:21:30 -0700

    Make text bigger and better

--- src/app/blog/first-post/page.tsx
+++ src/app/blog/first-post/page.tsx
@@ -1,7 +1,7 @@
 export default function FirstPost() {
     return (
-        <header className="flex justify-center">
-        <h1 className="font-montserrat">First Post</h1>
+        <header className="flex flex-col justify-center items-center">
+        <h1 className="font-montserrat text-4xl p-8">First Post</h1>
         <div className="description">
             <p className="font-montserrat">This is my first blog post.</p>
             <p className="font-montserrat">Here you will find articles about programming and technology.</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <header className="flex justify-center">
-      <h1 className="font-montserrat">Blog Today</h1>
+    <header className="flex flex-col justify-center items-center">
+      <h1 className="font-montserrat text-4xl p-8">Blog Today</h1>
       <div className="description font-montserrat">
         <p>Hi, Welcome to my Blog!</p>
         <p>Here you will find articles about programming and technology.</p>
