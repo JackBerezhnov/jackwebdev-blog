2627efdd4c05822500dcb76cb2ef84db4c91bed5
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-01 22:03:19 -0700

    Add Montserrat font

--- src/app/blog/first-post/page.tsx
+++ src/app/blog/first-post/page.tsx
@@ -1,10 +1,10 @@
 export default function FirstPost() {
     return (
         <header className="flex justify-center">
-        <h1>First Post</h1>
+        <h1 className="font-montserrat">First Post</h1>
         <div className="description">
-            <p>This is my first blog post.</p>
-            <p>Here you will find articles about programming and technology.</p>
+            <p className="font-montserrat">This is my first blog post.</p>
+            <p className="font-montserrat">Here you will find articles about programming and technology.</p>
         </div>
         </header>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <header className="flex justify-center">
-      <h1 className="montserrat">Blog Today</h1>
-      <div className="description">
+      <h1 className="font-montserrat">Blog Today</h1>
+      <div className="description font-montserrat">
         <p>Hi, Welcome to my Blog!</p>
         <p>Here you will find articles about programming and technology.</p>
       </div>
