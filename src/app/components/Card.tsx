0560937076b97064cec042e861b0994984f2a2ba
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-09 22:24:14 -0700

    Work on Card Styles

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,9 +1,11 @@
 const Card = () => {
     return(
-       <div className="card">
+       <div className="card flex justify-between items-center mt-4 w-96 shadow-lg">
             <img src="https://via.placeholder.com/150" alt="placeholder" />
-            <h2>Card Title</h2>
-            <p>Card Description</p>
+            <div className="pl-4">
+                <h2>Card Title</h2>
+                <p>Card Description</p>
+            </div>
        </div>  
     );
 }
