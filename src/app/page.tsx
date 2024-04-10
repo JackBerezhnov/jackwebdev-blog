05279ff06dea6a39695e7b1177c2442f78f9b523
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-09 21:12:50 -0700

    Make text a little bit bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <header className="flex flex-col justify-center items-center">
       <h1 className="font-montserrat text-4xl p-8">Blog Today</h1>
       <div className="description font-montserrat">
-        <p>Hi, Welcome to my Blog!</p>
-        <p>Here you will find articles about programming and technology.</p>
+        <p className="text-xl">Hi, Welcome to my Blog!</p>
+        <p className="text-xl">Here you will find articles about programming and technology.</p>
       </div>
     </header>
   );
