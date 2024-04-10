1452714a6e1ce6b02c3f1645ec4608f4cf686819
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-09 21:52:55 -0700

    Created Card Componnet

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return(
+       <div className="card">
+            <img src="https://via.placeholder.com/150" alt="placeholder" />
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+       </div>  
+    );
+}
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <header className="flex flex-col justify-center items-center">
-      <h1 className="font-montserrat text-4xl p-8">Blog Today</h1>
-      <div className="description font-montserrat">
-        <p className="text-xl">Hi, Welcome to my Blog!</p>
-        <p className="text-xl">Here you will find articles about programming and technology.</p>
-      </div>
-    </header>
+      <header className="flex flex-col justify-center items-center">
+        <h1 className="font-montserrat text-4xl p-8">Blog Today</h1>
+        <div className="description font-montserrat">
+          <p className="text-xl">Hi, Welcome to my Blog!</p>
+          <p className="text-xl">Here you will find articles about programming and technology.</p>
+        </div>
+        <Card />
+      </header>
   );
 }
