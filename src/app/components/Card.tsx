041aff4b8ac9115484cb3fbb50dbc0a0dc4b5c78
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-04-11 22:18:58 -0700

    play with a card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,6 +1,6 @@
 const Card = () => {
     return(
-       <div className="card flex justify-between items-center mt-4 w-96 shadow-lg">
+       <div className="card flex justify-between items-center mt-10 w-96 shadow-lg">
             <img src="https://via.placeholder.com/150" alt="placeholder" />
             <div className="pl-4">
                 <h2>Card Title</h2>
